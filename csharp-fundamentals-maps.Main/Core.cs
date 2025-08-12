9d17aeb4d74db9db87d66dc91636f05bf96b2a0b
Snorre Aldstedt <snorre.alstedt@experisacademy.no>
2025-08-12 13:55:42 +0200

    Pass all tests

--- csharp-fundamentals-maps.Main/Core.cs
+++ csharp-fundamentals-maps.Main/Core.cs
@@ -48,9 +48,9 @@ namespace csharp_fundamentals_maps.Main
 
         public string getValue(string key)
         {
-            
-           
-            return string.Empty;
+
+            Dictionary<string, string> map = createPerson();
+            return map[key];
 
 
         }
@@ -64,8 +64,7 @@ namespace csharp_fundamentals_maps.Main
          */
          public bool hasKey(Dictionary<string,string> dictionary, string isitthere)
          {
-            return false;
-            
+            return dictionary.ContainsKey(isitthere);            
          }
 
 
@@ -78,8 +77,14 @@ namespace csharp_fundamentals_maps.Main
          */
         public int getValueOrDefault(Dictionary<string,int> dictionary, string isitthere)
         {
-            return 0;
-
+            if (dictionary.ContainsKey(isitthere))
+            {
+                return dictionary[isitthere];
+            }
+            else 
+            {
+                return -1;
+            }
         }
 
 
@@ -105,7 +110,13 @@ namespace csharp_fundamentals_maps.Main
             map.Add(96, "nice");
             // Write your code below this comment...
 
-           
+            foreach(int n in numbers)
+            {
+                if (map.ContainsKey(n))
+                {
+                    results.Add(map[n]);
+                }
+            }
 
             //    // ...and above this comment
             return results;
